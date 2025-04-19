668379e165c878f8dfac3cc2b3063caf9e97b156
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2025-04-19 15:51:47 +0200

    added wandb

--- G2PT/scripts/pretrain.sh
+++ G2PT/scripts/pretrain.sh
@@ -5,22 +5,28 @@
 #SBATCH --time=12:00:00
 #SBATCH --output=../slurm_logs/g2pt_large_%j.out
 
+export WANDB_API_KEY="725d958326cb39d0ba89d73b557c294f85ecbf83"
+
 
 cd ..
 
 # Create log directories if they don't exist
 mkdir -p slurm_logs
-
+mkdir -p results/aig-small-topo # Ensure output dir exists
 
 module load 2024
 module load Anaconda3/2024.06-1
 
 source activate g2pt-aig
 
-
+echo "Starting training script..."
 srun python -u train.py configs/datasets/aig.py configs/networks/large.py \
     --dataset=aig \
     --ordering=topo \
     --batch_size=32 \
+    --out_dir=results/aig-small-topo \
+    --wandb_log=True
+
+echo "Training script finished."
 
 
