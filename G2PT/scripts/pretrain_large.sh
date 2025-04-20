5cb1a30bcf9ae33284c8f1aabc9dd73d2bd0a305
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2025-04-20 19:40:00 +0200

    large + bfs ready

--- G2PT/scripts/pretrain_large.sh
+++ G2PT/scripts/pretrain_large.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=g2pt_large
+#SBATCH --job-name=g2pt_l_bfs
 #SBATCH --partition=gpu_h100     # Specify the appropriate partition here
 #SBATCH --gpus=1
 #SBATCH --time=08:00:00
-#SBATCH --output=../slurm_logs/g2pt_large3_%j.out
+#SBATCH --output=../slurm_logs/g2pt_large_bfs_%j.out
 
 export WANDB_API_KEY="725d958326cb39d0ba89d73b557c294f85ecbf83"
 
@@ -12,7 +12,7 @@ cd ..
 
 # Create log directories if they don't exist
 mkdir -p slurm_logs
-mkdir -p results/aig-large3-topo # Ensure output dir exists
+mkdir -p results/aig-large-bfs # Ensure output dir exists
 
 module load 2024
 module load Anaconda3/2024.06-1
@@ -22,8 +22,7 @@ source activate g2pt-aig
 echo "Starting training script..."
 srun python -u train.py configs/datasets/aig.py configs/networks/large.py \
     --dataset=aig \
-    --ordering=topo \
-    --out_dir=results/aig-large3-topo \
+    --out_dir=results/aig-large-bfs \
     --wandb_log=True
 
 echo "Training script finished."
