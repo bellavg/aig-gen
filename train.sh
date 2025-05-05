98c0b04a2ce599e7a5dba1a7b471ff432ff89bb8
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2025-05-05 09:48:36 +0200

    new tryout train script

--- train.sh
+++ train.sh
@@ -2,13 +2,12 @@
 #SBATCH --job-name=g2pt_base_deg
 #SBATCH --partition=gpu_h100     # Specify the appropriate partition here
 #SBATCH --gpus=1
-#SBATCH --time=12:00:00
-#SBATCH --output=../slurm_logs/g2pt_base_deg_%j.out
+#SBATCH --time=00:59:00
+#SBATCH --output=./slurm_logs/g2pt_base_real_%j.out
 
 export WANDB_API_KEY="725d958326cb39d0ba89d73b557c294f85ecbf83"
 
 
-cd ..
 
 # Create log directories if they don't exist
 mkdir -p slurm_logs
@@ -18,6 +17,7 @@ module load 2024
 module load Anaconda3/2024.06-1
 
 source activate g2pt-aig
+cd G2PT
 # f"{dataset}-{model_name}-{ordering}-{num_augmentations}"
 echo "Starting training script..."
 srun python -u train.py configs/aig.py configs/base.py configs/train_aig.py configs/sample_aig.py
