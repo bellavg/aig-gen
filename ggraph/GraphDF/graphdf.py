e05105137f9931023bc3c832a5c0f67976c4e8d9
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2025-05-09 16:00:54 +0200

    fixed printing for df

--- ggraph/GraphDF/graphdf.py
+++ ggraph/GraphDF/graphdf.py
@@ -70,7 +70,8 @@ class GraphDF(Generator):
                 optimizer.step()
 
                 total_loss += loss.to('cpu').item()
-                print('Training iteration {} | loss {}'.format(batch, loss.to('cpu').item()))
+                if batch % 100 == 0:
+                    print('Training iteration {} | loss {}'.format(batch, loss.to('cpu').item()))
 
             avg_loss = total_loss / (batch + 1)
             print("Training | Average loss {}".format(avg_loss))
