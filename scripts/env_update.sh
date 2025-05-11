1f05b39322afb89dc862581fe1d317ed2b69da76
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2025-05-11 18:51:52 +0200

    build env

--- scripts/env_update.sh
+++ scripts/env_update.sh
@@ -3,12 +3,13 @@
 #SBATCH --partition=gpu_h100          # Or your specific H100 partition
 #SBATCH --gpus=1
 #SBATCH --time=00:59:00              # Initial requested time, adjust as needed
-#SBATCH --output=../slurm_logs/digress_aig_train_%j.out
+#SBATCH --output=../slurm_logs/digress_env_%j.out
 
 cd ..
+cd Digress
 
 # --- Configuration ---
-CONDA_ENV_NAME="g2pt-aig" # CHANGE THIS to your Conda environment name
+CONDA_ENV_NAME="digress" # CHANGE THIS to your Conda environment name
 
 
 
@@ -18,14 +19,21 @@ module load Anaconda3/2024.06-1 # Or your Anaconda module
 echo "Modules loaded."
 
 
-source activate g2pt-aig
-conda config --add channels conda-forge
-conda config --set channel_priority strict
-conda uninstall graph-tool pytorch # Or just one at a time to see
-conda install -c conda-forge graph-tool
-# Then install PyTorch, specifying your CUDA version if needed
-# Check the PyTorch website for the latest recommended command
-conda install pytorch torchvision torchaudio pytorch-cuda=11.X -c pytorch -c nvidia
+conda create -c conda-forge -n digress rdkit=2023.03.2 python=3.9
 
-# Deactivate conda environment
-conda deactivate
+conda activate digress
+
+conda install -c conda-forge graph-tool=2.45
+
+conda install -c "nvidia/label/cuda-11.8.0" cuda
+
+pip3 install torch==2.0.1 --index-url https://download.pytorch.org/whl/cu118
+
+pip install -r requirements.txt
+
+pip install -e .
+
+
+
+
+
