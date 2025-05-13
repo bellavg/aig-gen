b78b69a6171df240270e5f773357467721b0a81a
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2025-05-13 17:42:10 +0200

    fix script?

--- DiGress/digress.sh
+++ DiGress/digress.sh
@@ -38,7 +38,7 @@ echo "----------------------------------------"
 srun python -u src/main.py \
     +experiment=aig.yaml \
     dataset=aig \
-    +general.abs_path_to_project_root=./DiGress \
+    general.abs_path_to_project_root=./DiGress \
     # Add any other specific Hydra overrides here if needed, e.g.:
     # general.resume=outputs/YYYY-MM-DD/HH-MM-SS-aig_resume/checkpoints/last-epoch=XXXX.ckpt
     # train.n_epochs=6000 # To override the config file value
